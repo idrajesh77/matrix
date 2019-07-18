b6185802f7c444588819efc22d0ed24c55e5632c
idrajesh77 <idrajesh77@users.noreply.github.com>
2019-07-17 22:49:32 -0400

    add more logic to refactor and parameterize

--- matrix.py
+++ matrix.py
@@ -13,35 +13,61 @@ grid = \
         24	55	58	5	66	73	99	26	97	17
         21	36	23	9	75	0	76	44	20	45'''
 
-def findMax(matrix, n, m, adjNums):
-    
-    a = np.array([ int(x) for x in matrix.split() ]).reshape(n,m)
-    print (a)
-    b = a[::-1,]
+def countOfThrees(matrix, n, m, adjNums):
+    if adjNums <= 0: return ZeroDivisionError
 
-    maxprod = 0
+    count=0
+    a = np.array([ int(x) for x in matrix.split() ]).reshape(n,m)
+    
+    b = a[::-1,]
+    # print (b)
+    # print (np.shape(b))
 
     for i in range(n):
         for j in range(m):
-            r = np.prod( a[i][j:j+adjNums] )
-            c = np.prod( a[:,j][i:i+adjNums] )
+            if  len( a[i][j:j+adjNums]) == adjNums:
+                r = len( a[i][j:j+adjNums] )/adjNums
+            if len( a[:,j][i:i+adjNums] ) == adjNums:
+                c = len( a[:,j][i:i+adjNums] )/adjNums
             k = min(i,j)
-            d1 = np.prod( np.diagonal(a, j-i)[ k:k+adjNums ] )
-            d2 = np.prod( np.diagonal(b, j-i)[ k:k+adjNums ] )
+            if len(np.diagonal(a, j-i)[ k:k+adjNums ]) == adjNums:
+                d1 = len( np.diagonal(a, j-i)[ k:k+adjNums ] )/adjNums
+            if len( np.diagonal(b, j-i)[ k:k+adjNums ]) == adjNums:
+                d2 = len( np.diagonal(b, j-i)[ k:k+adjNums ] )/adjNums
+            count = sum([count,r,c,d1,d2])
+      
+    return count 
+
+def findMax(matrix, n, m, adjNums):
+    if adjNums <= 0: return ZeroDivisionError
+
+    maxprod = 0
+
+    a = np.array([ int(x) for x in matrix.split() ]).reshape(n,m)
+    
+    b = a[::-1,]
+    # print (b)
+    # print (np.shape(b))
+
+    for i in range(n):
+        for j in range(m):
+            if  len( a[i][j:j+adjNums]) == adjNums:
+                r = np.prod( a[i][j:j+adjNums] )
+            if len( a[:,j][i:i+adjNums] ) == adjNums:
+                c = np.prod( a[:,j][i:i+adjNums] )
+            k = min(i,j)
+            if len(np.diagonal(a, j-i)[ k:k+adjNums ]) == adjNums:
+                d1 = np.prod( np.diagonal(a, j-i)[ k:k+adjNums ] )
+            if len( np.diagonal(b, j-i)[ k:k+adjNums ]) == adjNums:
+                d2 = np.prod( np.diagonal(b, j-i)[ k:k+adjNums ] )
             maxprod = max([maxprod,r,c,d1,d2])
     
     return maxprod
 
-def countOfThrees(grid, n, m, adjNums):
-    maxcount=0
-
-    return maxcount
-
 def printMe():
+    count = countOfThrees(grid, 10, 10, 3)
+    print ("max count of 3 adjacent number: %s" % (count))
     maxm = findMax(grid, 10, 10, 3)
     print ("max product of 3 adjacent number: %s" % (maxm))
-
-    count = countOfThrees(grid, 10, 10, 3)
-    print ("max count of 3 adjacent number: %s" % (maxm))
 
 printMe()
